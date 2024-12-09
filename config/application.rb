053da32b18cbc51740908e0868d76f73df5268ec
Matheus Alves <Malves224@gmail.com>
2024-12-09 20:37:42 -0300

    feat(i18n): add Brazilian Portuguese locale and update default locale

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module AlwaysTodo
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.i18n.default_locale = :'pt-BR'
   end
 end
