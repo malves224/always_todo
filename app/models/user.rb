a5f3167f75486192a84e41cad8eb14a60252c61b
Matheus Alves <Malves224@gmail.com>
2024-12-10 11:50:26 -0300

    fix(User)format email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\z/ }
   validates :password, presence: true, length: { minimum: 6 }
   validates :name, presence: true
   validates :password_confirmation, presence: true, if: :password_required?
