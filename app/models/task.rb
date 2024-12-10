9a6d7dac6fdb8de4bf5419e4d7a7f7cfc71ba270
Matheus Alves <Malves224@gmail.com>
2024-12-09 21:58:47 -0300

    feat(Task) validates: min length to title, and max length to description

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true
+  validates :title, presence: true, length: { minimum: 3 }
+  validates :description, length: { maximum: 255 }, allow_blank: true
 
   enum status: { pending: 'pending', done: 'done' }
   validates :status, inclusion: { in: statuses.keys }
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'associations' do
     it { should belong_to(:user) }
+  end
+  describe 'validations' do
     it { should validate_presence_of(:title) }
+    it { should validate_length_of(:title).is_at_least(3) }
+    it { should validate_length_of(:description).is_at_most(255) }
   end
 end
