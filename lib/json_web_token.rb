5f0ca7a654f4d8b920236ba392882f7918213979
Matheus Alves <Malves224@gmail.com>
2024-12-09 21:07:54 -0300

    feat(Lib/JsonWebToken)#encode and #decode

---
+++ lib/json_web_token.rb
@@ -0,0 +1,13 @@
+class JsonWebToken
+  SECRET_KEY = ENV['jwt_secret']
+
+  def self.encode(payload, exp = 99_999.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def self.decode(token)
+    body = JWT.decode(token, SECRET_KEY)[0]
+    HashWithIndifferentAccess.new(body)
+  end
+end
